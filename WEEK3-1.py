a7aceea7617f324884160033271ba7c6d6b4dd61
Edward <genesisspirit11@gmail.com>
2018-04-25 20:12:04 +0800

    轉帳金額，可以用兩個變數tax1,tax2也可以直接print(tax)，但是答案會變成(x1,x2)

---
+++ WEEK3-1.py
@@ -0,0 +1,8 @@
+x1 = int(input())
+x2 = int(input())
+y = int(input())
+if y <= x1:
+    tax = (x1 - y,x2 + y)
+if y > x1:  # else better
+    tax = (x1 - x1, x2 + x1()
+print("Tax amount:$", tax1, tax2)
