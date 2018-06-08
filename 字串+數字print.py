cddc0c2dc34d2e405d89785ef7ed05714c17c063
Edward <genesisspirit11@gmail.com>
2018-06-08 09:25:59 +0800

    week4-1解答

--- 字串+數字print.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-b = int(input())
-print("the vector is (", str(a), ",", str(b), ")")
-print("the vector is (" + str(a) + ",", str(b) + ")")
-print("the vector is (" + str(a) + "," + str(b) + ")")
-print("my income is $", a, b)
-print("my income is $", a + b)
-print("my income is $" + str(a + b))  # 可去除＄後面空格
